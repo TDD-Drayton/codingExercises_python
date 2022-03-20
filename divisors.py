18e90bc51f2efc871e849c36a10b22ef1d5dc518
Toi Drayton <drayton.toi@gmail.com>
2022-03-20 17:52:07 -0400

    divisors

--- divisors.py
+++ divisors.py
@@ -1,3 +1,9 @@
 __author__='TDD-Drayton'
 
 num=int(input("Enter number and I will list the divisors: "))
+numList = list(range(1,num+1))
+divisorList = []
+
+for i in numList:
+    if num % i == 0:
+        divisorList.append(i)
